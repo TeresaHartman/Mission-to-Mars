d15698209c834a08e4baf3258c4b90dbbb7d9945
Teresa Hartman <teresa2h@users.campus>
2022-11-30 22:21:57 -0600

    finished module work

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
+# @app.route("/")
+# def index():
+#    mars = mongo.db.mars.find_one()
+#    #mars = ["here", "there", "everywhere"]
+#    #mars = "here"
+#    #print("--mars called--")
+#    #print(mars)
+#    return render_template("index.html", mars=mars)
 @app.route("/")
 def index():
-   #mars = mongo.db.mars.find_one()
-   mars = ["here", "there", "everywhere"]
-   print("--mars called--")
-   print(mars)
-   return render_template("index1.html", mars=mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
